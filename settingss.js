a019a27e309a3f7b4653b8c99feaf3a4abb73c0a
Mussa ngido <ngidomussa652@gmail.com>
2025-08-02 12:51:51 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " malvin~47kUBQxK#-G8x0afs4NZMfdhPMX9Nhzx9rdViSj2bmDBWFq-fxAQ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " malvin~x61z2KDC#3U8sdKMkxp5oqMFMKg1Iur8jNqwHE7ERiL2qGqDamGA",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "255697255656", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
